91e197ab97b9a7f133349695907b57d19693b2c5
kiyoshisatoshy <kiyoshisatoshy@gmail.com>
2021-11-15 16:44:10 +0300

    Create DI & Add api url

--- app/src/main/java/com/soft/crownedjester/programmingquotesapp/common/Constants.kt
+++ app/src/main/java/com/soft/crownedjester/programmingquotesapp/common/Constants.kt
@@ -1,4 +1,5 @@
 package com.soft.crownedjester.programmingquotesapp.common
 
 object Constants {
+    const val QUOTES_API_URL = "https://programmingquotesapi.azurewebsites.net/"
 }
--- app/src/main/java/com/soft/crownedjester/programmingquotesapp/di/AppModule.kt
+++ app/src/main/java/com/soft/crownedjester/programmingquotesapp/di/AppModule.kt
@@ -1,4 +1,37 @@
 package com.soft.crownedjester.programmingquotesapp.di
 
+import com.soft.crownedjester.programmingquotesapp.common.Constants
+import com.soft.crownedjester.programmingquotesapp.data.remote.IProgrammingQuotesApi
+import com.soft.crownedjester.programmingquotesapp.domain.repository.IQuotesRepository
+import com.soft.crownedjester.programmingquotesapp.domain.repository.QuotesRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideQuotesApi(): IProgrammingQuotesApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.QUOTES_API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(IProgrammingQuotesApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideQuotesRepository(
+        api: IProgrammingQuotesApi
+    ): IQuotesRepository {
+        return QuotesRepositoryImpl(api)
+    }
+
 }
