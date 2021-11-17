922bbf4349c57591915894a130fdd7fb6d9eec4b
kiyoshisatoshy <kiyoshisatoshy@gmail.com>
2021-11-18 02:38:56 +0300

    Add Share option-for test

--- app/src/main/java/com/soft/crownedjester/programmingquotesapp/presentation/quotes_list_screen/components/QuoteItem.kt
+++ app/src/main/java/com/soft/crownedjester/programmingquotesapp/presentation/quotes_list_screen/components/QuoteItem.kt
@@ -1,15 +1,17 @@
 package com.soft.crownedjester.programmingquotesapp.presentation.quotes_list_screen.components
 
+import android.content.Intent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import com.soft.crownedjester.programmingquotesapp.data.remote.dto.QuotesDto
@@ -19,6 +21,14 @@ fun QuoteItem(
     modifier: Modifier = Modifier,
     quote: QuotesDto
 ) {
+    val sendIntent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_TEXT, "${quote.quote} \n\bby ${quote.author}")
+        type = "text/plain"
+    }
+
+    val shareIntent = Intent.createChooser(sendIntent, null)
+    val context = LocalContext.current
 
     Card(
         modifier = modifier
@@ -31,12 +41,8 @@ fun QuoteItem(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(4.dp)
+                .padding(start = 6.dp, end = 10.dp)
         ) {
-//            IconButton(onClick = {  }) {
-//                Icon(imageVector = Icons.Sharp., contentDescription = )
-//
-//            }
             Text(
                 text = "\t${quote.quote}",
                 style = MaterialTheme.typography.body2,
@@ -46,19 +52,25 @@ fun QuoteItem(
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Row(
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(end = 8.dp, bottom = 4.dp, top = 2.dp)
-
             ) {
+                IconButton(modifier = Modifier
+                    .align(Alignment.TopStart),
+                    onClick = {
+                        context.startActivity(shareIntent)
+                    }) {
+                    Icon(imageVector = Icons.Outlined.Share, contentDescription = "Share")
+                }
+
                 Text(
+                    modifier = Modifier.align(Alignment.TopEnd),
                     text = quote.author,
                     style = MaterialTheme.typography.body2,
                     fontStyle = FontStyle.Normal
                 )
             }
-
         }
     }
 }
