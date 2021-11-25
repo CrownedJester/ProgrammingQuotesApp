79556135644714b9a1e752ad7f9b1a7e95b9dfa4
kiyoshisatoshy <kiyoshisatoshy@gmail.com>
2021-11-26 01:19:15 +0300

    Setup DI for Db

--- app/src/main/java/com/soft/crownedjester/programmingquotesapp/common/Constants.kt
+++ app/src/main/java/com/soft/crownedjester/programmingquotesapp/common/Constants.kt
@@ -2,4 +2,5 @@ package com.soft.crownedjester.programmingquotesapp.common
 
 object Constants {
     const val QUOTES_API_URL = "https://programming-quotes-api.herokuapp.com/"
+    const val DATABASE_NAME = "quotes_database"
 }
--- app/src/main/java/com/soft/crownedjester/programmingquotesapp/di/AppModule.kt
+++ app/src/main/java/com/soft/crownedjester/programmingquotesapp/di/AppModule.kt
@@ -1,9 +1,16 @@
 package com.soft.crownedjester.programmingquotesapp.di
 
+import android.app.Application
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.soft.crownedjester.programmingquotesapp.common.Constants
 import com.soft.crownedjester.programmingquotesapp.data.remote.IProgrammingQuotesApi
-import com.soft.crownedjester.programmingquotesapp.domain.repository.IQuotesRepository
-import com.soft.crownedjester.programmingquotesapp.domain.repository.QuotesRepositoryImpl
+import com.soft.crownedjester.programmingquotesapp.domain.repository.QuotesApiServicesRepository
+import com.soft.crownedjester.programmingquotesapp.domain.repository.QuotesApiServicesRepositoryImpl
+import com.soft.crownedjester.programmingquotesapp.features_quotes.data.data_source.QuoteDao
+import com.soft.crownedjester.programmingquotesapp.features_quotes.data.data_source.QuoteDatabase
+import com.soft.crownedjester.programmingquotesapp.features_quotes.repository.QuoteDatabaseRepository
+import com.soft.crownedjester.programmingquotesapp.features_quotes.repository.QuoteDatabaseRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,6 +22,24 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideQuoteDatabase(application: Application): RoomDatabase {
+        return Room.databaseBuilder(
+            application,
+            QuoteDatabase::class.java,
+            Constants.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideQuotesDatabaseRepository(
+        quoteDao: QuoteDao
+    ): QuoteDatabaseRepository {
+        return QuoteDatabaseRepositoryImpl(quoteDao)
+    }
 
     @Provides
     @Singleton
@@ -30,8 +55,8 @@ object AppModule {
     @Singleton
     fun provideQuotesRepository(
         api: IProgrammingQuotesApi
-    ): IQuotesRepository {
-        return QuotesRepositoryImpl(api)
+    ): QuotesApiServicesRepository {
+        return QuotesApiServicesRepositoryImpl(api)
     }
 
 }
