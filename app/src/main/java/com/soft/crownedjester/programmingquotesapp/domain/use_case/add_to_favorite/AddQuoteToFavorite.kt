5203075fa4a45cd9f646c897e2eb62330ef861a8
kiyoshisatoshy <kiyoshisatoshy@gmail.com>
2021-11-27 21:39:39 +0300

    Add Change Field option

--- app/src/main/java/com/soft/crownedjester/programmingquotesapp/domain/use_case/add_to_favorite/AddQuoteToFavorite.kt
+++ app/src/main/java/com/soft/crownedjester/programmingquotesapp/domain/use_case/add_to_favorite/AddQuoteToFavorite.kt
@@ -2,13 +2,13 @@ package com.soft.crownedjester.programmingquotesapp.domain.use_case.add_to_favor
 
 import com.soft.crownedjester.programmingquotesapp.domain.model.Quote
 import com.soft.crownedjester.programmingquotesapp.features_quotes.repository.QuoteDatabaseRepository
-import javax.inject.Inject
 
-class AddQuoteToFavorite (
+class AddQuoteToFavorite(
     private val dbRepository: QuoteDatabaseRepository
 ) {
 
     suspend operator fun invoke(quote: Quote) {
+        quote.isFavorite = true
         dbRepository.addQuote(quote)
     }
 }
--- app/src/main/java/com/soft/crownedjester/programmingquotesapp/domain/use_case/remove_favorite/RemoveQuoteFromFavorite.kt
+++ app/src/main/java/com/soft/crownedjester/programmingquotesapp/domain/use_case/remove_favorite/RemoveQuoteFromFavorite.kt
@@ -2,13 +2,13 @@ package com.soft.crownedjester.programmingquotesapp.domain.use_case.remove_favor
 
 import com.soft.crownedjester.programmingquotesapp.domain.model.Quote
 import com.soft.crownedjester.programmingquotesapp.features_quotes.repository.QuoteDatabaseRepository
-import javax.inject.Inject
 
-class RemoveQuoteFromFavorite (
+class RemoveQuoteFromFavorite(
     private val dbRepository: QuoteDatabaseRepository
 ) {
 
     suspend operator fun invoke(quote: Quote) {
+        quote.isFavorite = false
         dbRepository.deleteQuote(quote)
     }
 }
